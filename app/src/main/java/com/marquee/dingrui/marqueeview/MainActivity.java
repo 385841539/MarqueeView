48be20e557d298be6b27051f77f691bcf715d1a4
385841539@qq.com <385841539@qq.com>
2018-03-09 20:26:30 +0800

    完善功能。

--- app/src/main/java/com/marquee/dingrui/marqueeview/MainActivity.java
+++ app/src/main/java/com/marquee/dingrui/marqueeview/MainActivity.java
@@ -2,12 +2,59 @@ package com.marquee.dingrui.marqueeview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.marquee.dingrui.marqueeviewlib.MarqueeView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View btCpntrol;
+    private MarqueeView mV;
+    private View btStop;
+    private View textContent;
+    private View textContent2;
+    private MarqueeView mV2;
+    private MarqueeView mV3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btCpntrol = findViewById(R.id.bt_control);
+        textContent = findViewById(R.id.bt_control0);
+        textContent2 = findViewById(R.id.bt_control00);
+        mV = ((MarqueeView) findViewById(R.id.mv_main1));
+        mV2 = ((MarqueeView) findViewById(R.id.mv_main2));
+        mV3 = ((MarqueeView) findViewById(R.id.mv_main3));
+        textContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mV.setContent("111第一个跑马灯");
+                mV2.setContent("222第二个跑马灯");
+                mV3.setContent("333第三个跑马灯");
+            }
+        });
+        textContent2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mV.setContent("222快捷支付付款账号222");
+            }
+        });
+        btStop = findViewById(R.id.bt_control2);
+
+        btStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mV3.stopRoll();
+            }
+        });
+
+        btCpntrol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mV3.continueRoll();
+            }
+        });
     }
 }
--- marqueeviewlib/src/main/java/com/marquee/dingrui/marqueeviewlib/MarqueeView.java
+++ marqueeviewlib/src/main/java/com/marquee/dingrui/marqueeviewlib/MarqueeView.java
@@ -1,11 +1,14 @@
 package com.marquee.dingrui.marqueeviewlib;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.v7.widget.TintTypedArray;
 import android.text.TextPaint;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -20,20 +23,36 @@ import java.util.Random;
 public class MarqueeView extends View implements Runnable {
     private static final String TAG = "MarqueeView";
     Random random = new Random();//随机数,用于取得x,y坐标,颜色值，以及移动速度
-    private Paint paint;//画笔
+    private TextPaint paint;//画笔
     private int width;
-    private int height;
     private Context context;
-    private String string="快捷支付付款账号，需要先绑定为消费卡方可进行消费，点击本文本可查看已绑定的消费卡。   " +
-            "           快捷支付时间为每天00:00至21:30，且快捷付款金额要大于3.6元，请知晓。          " +
-            "    所有快捷支付的付款、绑卡操作都是直接跳转中国银联进行操作，A九用户可放心使用";
-    private float speed=1;
-    private int textColor;
-    private int textdistance;
-//    private boolean isResversable;
-    private boolean isContinuable;
-    private boolean isClickStop;
-    private float xLocation=0;
+    private String string;
+    private float speed = 1;
+    private int textColor = Color.BLACK;//文字颜色,默认黑色
+    private float textSize = 12;//文字颜色,默认黑色
+    private int textdistance = 50;//文字间距，dp单位
+    //    private boolean isResversable;
+    private int repetType = REPET_INTERVAL;
+    private boolean isClickStop = false;
+    private boolean isResetLocation = true;//默认为true
+    private float xLocation = 0;
+    private String black_count = "";//间距转化成空格
+
+    public static final int REPET_ONCETIME = 0;//一次结束
+    public static final int REPET_INTERVAL = 1;//一次结束以后，再继续第二次
+    public static final int REPET_CONTINUOUS = 2;//紧接着 滚动第二次
+    private Rect rect;
+
+    private int repetCount = 0;
+
+    private boolean resetInit = true;
+    private float startLocationDistance = 1.0f;//开始的位置选取，百分比来的，距离左边，0~1，0代表不间距，1的话代表，从右面，1/2代表中间。
+    private boolean isRoll = false;
+    private Thread thread;
+    private String content = "";
+    private int contentWidth;
+    private float oneBlack_width;//空格的宽度
+    private float textHeight;
 
 
     public MarqueeView(Context context) {
@@ -47,19 +66,38 @@ public class MarqueeView extends View implements Runnable {
     public MarqueeView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.context = context;
-        initpaint();
-        new Thread(this).start();//开启死循环线程让文字动起来
 
+        initattrs(attrs);
+        initpaint();
     }
+
+    @SuppressLint("RestrictedApi")
+    private void initattrs(AttributeSet attrs) {
+        TintTypedArray tta = TintTypedArray.obtainStyledAttributes(getContext(), attrs,
+                R.styleable.MarqueeView);
+
+        textColor = tta.getColor(R.styleable.MarqueeView_marqueeview_text_color, textColor);
+        isClickStop = tta.getBoolean(R.styleable.MarqueeView_marqueeview_isclickalbe_stop, isClickStop);
+        isResetLocation = tta.getBoolean(R.styleable.MarqueeView_marqueeview_is_resetLocation, isResetLocation);
+        speed = tta.getFloat(R.styleable.MarqueeView_marqueeview_text_speed, speed);
+        textSize = tta.getFloat(R.styleable.MarqueeView_marqueeview_text_size, textSize);
+        textdistance = tta.getInteger(R.styleable.MarqueeView_marqueeview_text_distance, textdistance);
+        startLocationDistance = tta.getFloat(R.styleable.MarqueeView_marqueeview_text_startlocationdistance, startLocationDistance);
+        repetType = tta.getInt(R.styleable.MarqueeView_marqueeview_repet_type, repetType);
+        tta.recycle();
+    }
+
 
     /**
      * 刻字机修改
      */
     private void initpaint() {
 
+        rect = new Rect();
         paint = new TextPaint(Paint.ANTI_ALIAS_FLAG);//初始化文本画笔
-        paint.setColor(Color.RED);//文字颜色值,可以不设定
-        paint.setTextSize(dp2px(11));//文字大小
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(textColor);//文字颜色值,可以不设定
+        paint.setTextSize(dp2px(textSize));//文字大小
 
     }
 
@@ -71,24 +109,87 @@ public class MarqueeView extends View implements Runnable {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if (resetInit) {
+            setTextDistance(textdistance);
+
+            if (startLocationDistance < 0) {
+                startLocationDistance = 0;
+            } else if (startLocationDistance > 1) {
+                startLocationDistance = 1;
+            }
+            xLocation = getWidth() * startLocationDistance;
+            Log.e(TAG, "onMeasure: --- " + xLocation);
+            resetInit = false;
+        }
+
+
+        //需要判断滚动模式的
+        switch (repetType) {
+            case REPET_ONCETIME:
+
+                if (contentWidth < (-xLocation)) {
+                    //也就是说文字已经到头了
+//                    此时停止线程就可以了
+                    stopRoll();
+                }
+                break;
+            case REPET_INTERVAL:
+                if (contentWidth <= (-xLocation)) {
+                    //也就是说文字已经到头了
+                    xLocation = getWidth();
+                }
+                break;
+
+            case REPET_CONTINUOUS:
+
+                if (xLocation < 0) {
+                    int beAppend = (int) ((-xLocation) / contentWidth);
+
+                    Log.e(TAG, "onDraw: ---" + contentWidth + "--------" + (-xLocation) + "------" + beAppend);
+
+                    if (beAppend >= repetCount) {
+                        repetCount++;
+                        //也就是说文字已经到头了
+//                    xLocation = speed;//这个方法有问题，所以采取了追加字符串的 方法
+                        string = string + content;
+                    }
+                }
+                //此处需要判断的xLocation需要加上相应的宽度
+                break;
+
+            default:
+                //默认一次到头好了
+                if (contentWidth < (-xLocation)) {
+                    //也就是说文字已经到头了
+//                    此时停止线程就可以了
+                    stopRoll();
+                }
+                break;
+        }
+
 
         //把文字画出来
         if (string != null) {
-            Log.e(TAG, "onDraw: " );
-            canvas.drawText(string,xLocation, getMeasuredHeight() / 2 + height / 2, paint);
+            canvas.drawText(string, xLocation, getHeight() / 2 + textHeight / 2, paint);
+
 
         }
 
     }
 
+    public void setRepetType(int repetType) {
+        this.repetType = repetType;
+        resetInit = true;
+        setContent(content);
+    }
+
+
     @Override
     public void run() {
-        while (true) {
+        while (isRoll && !TextUtils.isEmpty(content)) {
             try {
                 Thread.sleep(10);
-                if (string != null) {
-                    xLocation=xLocation-speed;
-                }
+                xLocation = xLocation - speed;
                 postInvalidate();//每隔500毫秒重绘视图
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -101,14 +202,31 @@ public class MarqueeView extends View implements Runnable {
     /**
      * 继续滚动
      */
-    private void continueRoll() {
+    public void continueRoll() {
+        if (!isRoll) {
+            if (thread != null) {
+                thread.interrupt();
 
+                thread = null;
+            }
+
+            isRoll = true;
+            thread = new Thread(this);
+            thread.start();//开启死循环线程让文字动起来
+
+        }
     }
 
     /**
      * 停止滚动
      */
-    private void stopRoll() {
+    public void stopRoll() {
+        isRoll = false;
+        if (thread != null) {
+            thread.interrupt();
+            thread = null;
+        }
+
     }
 
     /**
@@ -126,8 +244,8 @@ public class MarqueeView extends View implements Runnable {
      *
      * @param isContinuable
      */
-    private void setContinueble(boolean isContinuable) {
-        this.isContinuable = isContinuable;
+    private void setContinueble(int isContinuable) {
+        this.repetType = isContinuable;
     }
 
 //    /**
@@ -141,13 +259,63 @@ public class MarqueeView extends View implements Runnable {
 
 
     /**
-     * 设置文字间距
+     * 设置文字间距 避免代码过多，该方法必须要在 设置文本之前设置，否则不成功
      *
-     * @param textdistance
+     * @param textdistance2
      */
-    private void setTextDistance(int textdistance) {
+    private void setTextDistance(int textdistance2) {
 
-        this.textdistance = textdistance;
+
+        //设置之后就需要初始化了
+
+        String black = " ";
+        oneBlack_width = getBlacktWidth();//空格的宽度
+        textdistance2 = dp2px(textdistance2);
+        int count = (int) (textdistance2 / oneBlack_width);//空格个数，有点粗略，有兴趣的朋友可以精细
+
+        if (count == 0) {
+            count = 1;
+        }
+
+        textdistance = (int) (oneBlack_width * count);
+        black_count = "";
+        for (int i = 0; i <= count; i++) {
+            black_count = black_count + black;//间隔字符串
+        }
+    }
+
+    private float getBlacktWidth() {
+        String text1 = "en en";
+        String text2 = "enen";
+        return getContentWidth(text1) - getContentWidth(text2);
+
+    }
+
+    private float getContentWidth(String black) {
+        if (black == null || black == "") {
+            return 0;
+        }
+
+        if (rect == null) {
+            rect = new Rect();
+        }
+        paint.getTextBounds(black, 0, black.length(), rect);
+        textHeight = getContentHeight();
+        return rect.width();
+    }
+
+    /**
+     * http://blog.csdn.net/u014702653/article/details/51985821
+     * 详细解说了
+     *
+     * @param
+     * @return
+     */
+    private float getContentHeight() {
+
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+        return Math.abs((fontMetrics.bottom - fontMetrics.top)) / 2;
+
     }
 
     /**
@@ -156,7 +324,22 @@ public class MarqueeView extends View implements Runnable {
      * @param textColor
      */
     private void setTextColor(int textColor) {
-        this.textColor = textColor;
+        if (textColor != 0) {
+            this.textColor = textColor;
+            paint.setColor(textColor);//文字颜色值,可以不设定
+        }
+    }
+
+    /**
+     * 设置文字大小
+     *
+     * @param textSize
+     */
+    private void setTextSize(float textSize) {
+        if (textSize > 0) {
+            this.textSize = textSize;
+            paint.setColor(dp2px(textSize));//文字颜色值,可以不设定
+        }
     }
 
     /**
@@ -177,15 +360,85 @@ public class MarqueeView extends View implements Runnable {
      * @param strings
      */
     public void setContent(List<String> strings) {
+        setTextDistance(textdistance);
+        String temString = "";
+
+        if (strings != null && strings.size() != 0) {
+
+            for (int i = 0; i < strings.size(); i++) {
+
+                temString = strings.get(i) + black_count;
+            }
+
+        }
+        setContent(temString);
+    }
+
+
+    /**
+     * 设置滚动的条目内容  字符串形式的
+     *
+     * @param
+     */
+    public void setContent(String content2) {
+        if (isResetLocation) {
+            xLocation = getWidth() * startLocationDistance;
+        }
+
+        if (TextUtils.isEmpty(content2)) {
+            return;
+        }
+        if (!content2.endsWith(black_count)) {
+            content2 = content2 + black_count;//避免没有后缀
+        }
+        this.content = content2;
+        //这里需要计算宽度啦，当然要根据模式来搞
+        if (repetType == REPET_CONTINUOUS) {
+//如果说是循环的话，则需要计算 文本的宽度 ，然后再根据屏幕宽度 ， 看能一个屏幕能盛得下几个文本
+
+            contentWidth = (int) (getContentWidth(content) + textdistance);//以空格结尾的，空格是不计算进去的
+            //从0 开始计算了，否则会重新 ， 否则到最后 会跨不过这个坎
+            repetCount=0;
+            int contentCount = (getWidth() / contentWidth) + 2;
+//            if (contentWidth > getWidth()) {//比屏幕宽，则需要两个；
+//                contentCount=2;
+//            }else {
+//                contentCount +1
+//
+//            }
+
+            this.string = "";
+            for (int i = 0; i <= contentCount; i++) {
+                this.string = this.string + this.content;
+            }
+
+        } else {
+            if (xLocation<0&&repetType==REPET_ONCETIME){
+                if (-xLocation>contentWidth){
+                    xLocation=getWidth()*startLocationDistance;
+                }
+            }
+            contentWidth = (int) getContentWidth(content);
+
+            this.string = content2;
+        }
+
+        if (!isRoll) {
+            continueRoll();
+        }
+
 
     }
 
     /**
-     * |设置滚动的条目内容  字符串形式的
-     *
-     * @param string
+     * 从新添加内容的时候，是否初始化位置
+     * @param isReset
      */
-    public void setContent(String string) {
-        this.string = string;
+    private void setResetLocation(boolean isReset) {
+        isResetLocation=isReset;
+    }
+
+    public void appendContent(String appendContent) {
+//有兴趣的朋友可以自己完善，在现有的基础之上，静默追加新的 公告
     }
 }
